9dc7291b3724cec7c53682532e706ff678963c68
Simon Larsson <simon.larsson@seavus.com>
2020-01-17 13:19:48 +0100

    added applications concept

---
+++ masochism/applications.py
@@ -0,0 +1,21 @@
+def DepressNet(include_top=True,
+               weights=None,
+               input_tensor=None,
+               input_shape=None,
+               pooling=None,
+               classes=1000,
+               **kwargs):
+    """ Deep Pre-Activation Residual Network
+    """
+
+    raise NotImplementedError('Only a concept so far.')
+
+def DeficientNet(include_top=True,
+                 weights=None,
+                 input_tensor=None,
+                 input_shape=None,
+                 pooling=None,
+                 classes=1000,
+                 **kwargs):
+
+    raise NotImplementedError('Only a concept so far.')
